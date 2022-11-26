1b9b4385dee56db10175f443f77c4d43de3bc33f
s23210 <73021331+s23210@users.noreply.github.com>
2022-11-26 13:42:38 +0100

    Initial commit

---
+++ src/main/java/pl/pjatk/EatGood/EatGoodApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.EatGood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatGoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/EatGood/EatGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.EatGood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
