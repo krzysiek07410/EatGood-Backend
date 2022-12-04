c39e6b68a8fa050652783757a92c5bfac25e4035
s23210 <73021331+s23210@users.noreply.github.com>
2022-12-04 17:19:05 +0100

    Create GenericRecipe

---
+++ src/main/java/pl/pjatk/EatGood/domain/GenericRecipe.java
@@ -0,0 +1,63 @@
+package pl.pjatk.EatGood.domain;
+
+import javax.persistence.Id;
+
+public class GenericRecipe {
+    @Id
+    private Integer id;
+    private String title;
+    private String image;
+    private String imageType;
+
+    public GenericRecipe(Integer id, String title, String image, String imageType) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.imageType = imageType;
+    }
+
+    public GenericRecipe() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    @Override
+    public String toString() {
+        return "GenericRecipe{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", image='" + image + '\'' +
+                ", imageType='" + imageType + '\'' +
+                '}';
+    }
+}
