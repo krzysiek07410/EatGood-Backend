58d6b2229b0ea710d253b6389355f66bf9b4140c
s23210 <73021331+s23210@users.noreply.github.com>
2023-01-16 18:11:41 +0100

    Add rest of unit tests in RecipeServiceTest

--- src/test/java/pl/pjatk/EatGood/RecipeServiceTest.java
+++ src/test/java/pl/pjatk/EatGood/RecipeServiceTest.java
@@ -1,14 +1,17 @@
 package pl.pjatk.EatGood;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import pl.pjatk.EatGood.domain.GenericRecipe;
 import pl.pjatk.EatGood.domain.GenericRecipeList;
 import pl.pjatk.EatGood.domain.Recipe;
@@ -19,8 +22,7 @@ import java.io.FileInputStream;
 import java.util.LinkedHashMap;
 import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -36,7 +38,6 @@ class RecipeServiceTest {
     @Mock
     private RestTemplate restTemplate;
     private RecipeService recipeService;
-
 
     @BeforeEach
     void setup() {
@@ -56,18 +57,18 @@ class RecipeServiceTest {
         assertEquals(expectedRecipes, actualRecipes);
     }
 
-//    @Test
-//    public void getRecipesInformationTest() {
-//        String ids = "123,456";
-//        RecipeList expectedRecipes = new RecipeList();
-//        ResponseEntity<RecipeList> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
-//        when(restTemplate.exchange(eq(API_URL + "/recipes/information?ids=" + ids),
-//                eq(HttpMethod.GET), any(HttpEntity.class), eq(RecipeList.class))).thenReturn(response);
-//
-//        RecipeList actualRecipes = recipeService.getRecipesInformation(ids);
-//
-//        assertEquals(expectedRecipes, actualRecipes);
-//    }
+    @Test
+    public void getRecipesInformationTest() {
+        String ids = "123,456";
+        Recipe[] expectedRecipes = new Recipe[]{};
+        ResponseEntity<Recipe[]> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
+        when(restTemplate.exchange(eq(API_URL + "/recipes/informationBulk?ids=" + ids),
+                eq(HttpMethod.GET), any(HttpEntity.class), eq(Recipe[].class))).thenReturn(response);
+
+        RecipeList actualRecipes = recipeService.getRecipesInformation(ids);
+
+        assertArrayEquals(expectedRecipes, actualRecipes.getRecipes());
+    }
 
     @Test
     public void getRecipesByCaloriesTest() {
@@ -123,51 +124,51 @@ class RecipeServiceTest {
         assertEquals(expectedRecipes, actualRecipes);
     }
 
-    //    @Test
-//    public void getRecipesByQueryAndCaloriesTest() {
-//        String query = "chicken";
-//        long minCalories = 100;
-//        long maxCalories = 1000;
-//        GenericRecipeList expectedRecipes = new GenericRecipeList();
-//        ResponseEntity<GenericRecipeList> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
-//        when(restTemplate.exchange(eq(API_URL + "/recipes/complexSearch?query=" + query + "&minCalories=" + minCalories + "&maxCalories="  + maxCalories),
-//                eq(HttpMethod.GET), any(HttpEntity.class), eq(GenericRecipeList.class))).thenReturn(response);
-//
-//        GenericRecipeList actualRecipes = recipeService.getRecipesByQueryAndCalories(query, minCalories, maxCalories);
-//
-//        assertEquals(expectedRecipes, actualRecipes);
-//    }
+    @Test
+    public void getRecipesByQueryAndCaloriesTest() {
+        String query = "chicken";
+        long min = 100;
+        long max = 1000;
+        GenericRecipeList expectedRecipes = new GenericRecipeList();
+        ResponseEntity<GenericRecipeList> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
+        when(restTemplate.exchange(eq(API_URL + "/recipes/complexSearch?query=" + query + "&minCalories=" + min + "&maxCalories="  + max),
+                eq(HttpMethod.GET), any(HttpEntity.class), eq(GenericRecipeList.class))).thenReturn(response);
 
-//    @Test
-//    public void getRecipesByDietAndCaloriesTest() {
-//        String diet = "vegetarian";
-//        long minCalories = 100;
-//        long maxCalories = 1000;
-//        GenericRecipeList expectedRecipes = new GenericRecipeList();
-//        ResponseEntity<GenericRecipeList> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
-//        when(restTemplate.exchange(eq(API_URL + "/recipes/complexSearch?diet=" + diet + "&minCalories=" + minCalories + "&maxCalories="  + maxCalories),
-//                eq(HttpMethod.GET), any(HttpEntity.class), eq(GenericRecipeList.class))).thenReturn(response);
-//
-//        GenericRecipeList actualRecipes = recipeService.getRecipesByDietAndCalories(diet, minCalories, maxCalories);
-//
-//        assertEquals(expectedRecipes, actualRecipes);
-//    }
+        GenericRecipeList actualRecipes = recipeService.getRecipesByQueryAndCalories(query, min, max);
 
-//    @Test
-//    public void getRecipesByDietAndCaloriesTest() {
-//        String query = "chicken";
-//        String diet = "vegetarian";
-//        long minCalories = 100;
-//        long maxCalories = 1000;
-//        GenericRecipeList expectedRecipes = new GenericRecipeList();
-//        ResponseEntity<GenericRecipeList> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
-//        when(restTemplate.exchange(eq(API_URL + "/recipes/complexSearch?query=" + query + "&diet=" + diet + "&minCalories=" + minCalories + "&maxCalories="  + maxCalories),
-//                eq(HttpMethod.GET), any(HttpEntity.class), eq(GenericRecipeList.class))).thenReturn(response);
-//
-//        GenericRecipeList actualRecipes = recipeService.getRecipesByQueryAndDietAndCalories(query, diet, minCalories, maxCalories);
-//
-//        assertEquals(expectedRecipes, actualRecipes);
-//    }
+        assertEquals(expectedRecipes, actualRecipes);
+    }
+
+    @Test
+    public void getRecipesByDietAndCaloriesTest() {
+        String diet = "vegetarian";
+        long minCalories = 100;
+        long maxCalories = 1000;
+        GenericRecipeList expectedRecipes = new GenericRecipeList();
+        ResponseEntity<GenericRecipeList> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
+        when(restTemplate.exchange(eq(API_URL + "/recipes/complexSearch?diet=" + diet + "&minCalories=" + minCalories + "&maxCalories="  + maxCalories),
+                eq(HttpMethod.GET), any(HttpEntity.class), eq(GenericRecipeList.class))).thenReturn(response);
+
+        GenericRecipeList actualRecipes = recipeService.getRecipesByDietAndCalories(diet, minCalories, maxCalories);
+
+        assertEquals(expectedRecipes, actualRecipes);
+    }
+
+    @Test
+    public void getRecipesByQueryDietAndCaloriesTest() {
+        String query = "chicken";
+        String diet = "vegetarian";
+        long minCalories = 100;
+        long maxCalories = 1000;
+        GenericRecipeList expectedRecipes = new GenericRecipeList();
+        ResponseEntity<GenericRecipeList> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
+        when(restTemplate.exchange(eq(API_URL + "/recipes/complexSearch?query=" + query + "&diet=" + diet + "&minCalories=" + minCalories + "&maxCalories="  + maxCalories),
+                eq(HttpMethod.GET), any(HttpEntity.class), eq(GenericRecipeList.class))).thenReturn(response);
+
+        GenericRecipeList actualRecipes = recipeService.getRecipesByQueryAndDietAndCalories(query, diet, minCalories, maxCalories);
+
+        assertEquals(expectedRecipes, actualRecipes);
+    }
 
     @Test
     public void getRandomRecipeTest() {
@@ -181,15 +182,32 @@ class RecipeServiceTest {
         assertEquals(expectedRecipes, actualRecipes);
     }
 
-//    @Test
-//    public void getHeadersTest() {
-//        GenericRecipeList expectedRecipes = new GenericRecipeList();
-//        ResponseEntity<GenericRecipeList> response = new ResponseEntity<>(expectedRecipes, HttpStatus.OK);
-//        when(restTemplate.exchange(eq(API_URL + "/recipes/complexSearch?query=xyz"),
-//                eq(HttpMethod.GET), any(HttpEntity.class), eq(GenericRecipeList.class))).thenReturn(response);
-//
-//        LinkedHashMap<String, Integer> actualHeaders = recipeService.getHeaders();
-//
-//        assertEquals(response.getHeaders(), actualHeaders);
-//    }
+    @Test
+    public void getHeadersTest() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-ratelimit-requests-limit", "10");
+        headers.set("x-ratelimit-requests-remaining", "99");
+        headers.set("x-ratelimit-results-limit", "100");
+        headers.set("x-ratelimit-results-remaining", "997");
+        headers.set("x-ratelimit-tinyrequests-limit", "420");
+        headers.set("x-ratelimit-tinyrequests-remaining", "666");
+        ResponseEntity<GenericRecipeList> response = new ResponseEntity<>(headers, HttpStatus.OK);
+        when(restTemplate.exchange(eq(API_URL + "/recipes/complexSearch?query=xyz"),
+                eq(HttpMethod.GET), any(HttpEntity.class), eq(GenericRecipeList.class))).thenReturn(response);
+
+        LinkedHashMap<String, Integer> mapLimits = recipeService.getHeaders();
+
+        assertTrue(mapLimits.containsKey("x-ratelimit-requests-limit"));
+        assertTrue(mapLimits.containsKey("x-ratelimit-requests-remaining"));
+        assertTrue(mapLimits.containsKey("x-ratelimit-results-limit"));
+        assertTrue(mapLimits.containsKey("x-ratelimit-results-remaining"));
+        assertTrue(mapLimits.containsKey("x-ratelimit-tinyrequests-limit"));
+        assertTrue(mapLimits.containsKey("x-ratelimit-tinyrequests-remaining"));
+        assertNotNull(mapLimits.get("x-ratelimit-requests-limit"));
+        assertNotNull(mapLimits.get("x-ratelimit-requests-remaining"));
+        assertNotNull(mapLimits.get("x-ratelimit-results-limit"));
+        assertNotNull(mapLimits.get("x-ratelimit-results-remaining"));
+        assertNotNull(mapLimits.get("x-ratelimit-tinyrequests-limit"));
+        assertNotNull(mapLimits.get("x-ratelimit-tinyrequests-remaining"));
+    }
 }
