495746a4beb990ff7a2e97d2f038811a267fc464
Maciej Zagórski <s23575@pjwstk.edu.pl>
2023-01-17 19:45:57 +0100

    Resolved config problems

--- src/main/java/pl/pjatk/EatGood/config/WebConfig.java
+++ src/main/java/pl/pjatk/EatGood/config/WebConfig.java
@@ -1,9 +1,7 @@
 package pl.pjatk.EatGood.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -18,4 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/");
+        registry.addResourceHandler("/csrf");
+    }
 }
