e5f4a89128c5fe729a805c8775ad81057294b48d
s23210 <73021331+s23210@users.noreply.github.com>
2023-01-18 00:22:57 +0100

    Add Swagger Api for FilterErrorController

--- src/main/java/pl/pjatk/EatGood/controller/FilterErrorController.java
+++ src/main/java/pl/pjatk/EatGood/controller/FilterErrorController.java
@@ -1,6 +1,10 @@
 package pl.pjatk.EatGood.controller;
 
 import io.jsonwebtoken.ExpiredJwtException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,21 @@ import pl.pjatk.EatGood.exceptionshandlers.TokenHeaderException;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Objects;
 
+@Api(value = "Error Controller", description = "Error Controller that handles exceptions")
 @Controller
 public class FilterErrorController implements ErrorController {
     @RequestMapping("/error")
+    @ApiOperation(
+            value = "Exception  handler",
+            notes = "Handles exceptions",
+            response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public void handleError(HttpServletRequest request) throws Throwable {
         String exceptionString = new String();
         if (request.getAttribute("javax.servlet.error.exception") != null) {
