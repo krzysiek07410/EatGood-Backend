8824a448d12b9db1858b80e6831c94ab34b43348
s23210 <73021331+s23210@users.noreply.github.com>
2023-01-17 11:49:49 +0100

    Add ApiModel for FavouriteRecipe

--- src/main/java/pl/pjatk/EatGood/domain/FavouriteRecipe.java
+++ src/main/java/pl/pjatk/EatGood/domain/FavouriteRecipe.java
@@ -1,13 +1,17 @@
 package pl.pjatk.EatGood.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import java.util.Set;
 
+@ApiModel(value = "FavouriteRecipe", description = "A favourite recipe")
 @Entity
 public class FavouriteRecipe extends GenericRecipe {
+    @ApiModelProperty(value = "Set of users that added this recipe to their favorites")
     @ManyToMany(mappedBy = "favouriteRecipeSet")
     @JsonBackReference
     private Set<User> userSet;
